0eae5b1439685121a3dcd8c2cdcc04a6854b7f67
Guilherme Marques <guiferreira_94@hotmail.com>
2021-09-04 11:41:37 -0300

    Curso de React usando Drag and Drop

---
+++ src/styles/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+export default function Header() {
+  return(
+    <Container>
+      <h1>React Drag and Drop</h1>
+    </Container>
+  );
+}
